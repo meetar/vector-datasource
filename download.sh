55b8824e61723e1bbcf1091defa7577d868f2760
Peter Richardson <peter@mapzen.com>
2014-05-29 15:31:58 -0400

    download and process osm layers

---
+++ download.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+
+cat filelist.txt | while read line; do 
+	cd data
+    echo $line # or whatever you want to do with the $line variable
+    #curl -O $line
+    filename=${line##*/}
+    unzip -j $filename
+    base=${filename%.*}
+    echo $base
+    cd ..
+done
---
+++ shp2pgsql.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# After unpacking the contents of each zip archive, pipe this script to psql.
+#
+shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ./data/ne_10m_lakes.shp ne_10m_lakes
+shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ./data/ne_10m_ocean.shp ne_10m_ocean
+shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ./data/ne_10m_playas.shp ne_10m_playas
+shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ./data/ne_110m_lakes.shp ne_110m_lakes
+shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ./data/ne_110m_ocean.shp ne_110m_ocean
+shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ./data/ne_110m_ocean.shp ne_110m_ocean.shp
+shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ./data/ne_50m_lakes.shp ne_50m_lakes
+shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ./data/ne_50m_ocean.shp ne_50m_ocean
+shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ./data/ne_50m_playas.shp ne_50m_playas
+shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ./data/ne_50m_urban_areas.shp ne_50m_urban_areas
+shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ./data/ne_10m_urban_areas.shp ne_10m_urban_areas
+shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ./data/ne_10m_parks_and_protected_lands_area.shp ne_10m_parks_and_protected_lands
+shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ./data/simplified_land_polygons.shp simplified_land_polygons
+shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ./data/land_polygons.shp land_polygons
+shp2pgsql -dID -s 900913 -W Windows-1252 -g the_geom ./data/water_polygons.shp water_polygons
