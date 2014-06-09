3ceb6c674d63fef2d1154abe5ed99976c0157a76
Peter Richardson <github@zoomy.net>
2014-06-09 14:00:19 -0400

    replaced overwrite flag with "never overwrite" flag

--- download.sh
+++ download.sh
@@ -6,7 +6,7 @@ cat filelist.txt | while read line; do
     echo $line # or whatever you want to do with the $line variable
     curl -O $line
     filename=${line##*/}
-    unzip -j -o $filename
+    unzip -j -n $filename
     base=${filename%.*}
     echo $base
     cd ..
